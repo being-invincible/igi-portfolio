058fd0f259e6091d863cf706de9d3c7c2a1f7503
Ha__sh <anelegistfrom98@yahoo.com>
2023-01-15 01:15:38 +0530

    Commit-04

--- components/About.jsx
+++ components/About.jsx
@@ -17,7 +17,7 @@ function About() {
           </h2>
           <p className='mt-4 font-medium text-justify text-sm text-platinum sm:mt-4 sm:block'>Imran Gulf Industries F. Co. is a leading provider of tailor-made solutions and premium quality products, founded in Saudi Arabia with a decade-long track record of excellence. We understand the importance of providing our customers with the very best in terms of quality, comfort, and style.</p>
 
-          <p class="hidden font-medium text-justify text-sm text-platinum sm:mt-4 sm:block">Our specialization in supplying hotel linens to the hospitality industry and luxury bed linens to discerning households reflects our commitment to meeting the highest standards of elegance, luxury, and beauty. Whether you are a hotelier looking to elevate your guests' experience, or a household with an eye for luxury and comfort, we have the perfect solutions for you.</p>
+          <p class="font-medium text-justify text-sm text-platinum sm:mt-4 sm:block">Our specialization in supplying hotel linens to the hospitality industry and luxury bed linens to discerning households reflects our commitment to meeting the highest standards of elegance, luxury, and beauty. Whether you are a hotelier looking to elevate your guests' experience, or a household with an eye for luxury and comfort, we have the perfect solutions for you.</p>
 
           <div class="mt-4 md:mt-6">
           <a
